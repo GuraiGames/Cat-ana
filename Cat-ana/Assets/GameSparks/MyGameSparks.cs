2d304c120b3072c6ce0a055715cc08667fdd3992
Guillem Sunyer Caldú <gsunyercaldu@gmail.com>
2017-06-13 23:14:41 +0200

    Basic folder organization

---
+++ Cat-ana/Assets/GameSparks/MyGameSparks.cs
@@ -0,0 +1,22 @@
+#pragma warning disable 612,618
+#pragma warning disable 0114
+#pragma warning disable 0108
+
+using System;
+using System.Collections.Generic;
+using GameSparks.Core;
+using GameSparks.Api.Requests;
+using GameSparks.Api.Responses;
+
+//THIS FILE IS AUTO GENERATED, DO NOT MODIFY!!
+//THIS FILE IS AUTO GENERATED, DO NOT MODIFY!!
+//THIS FILE IS AUTO GENERATED, DO NOT MODIFY!!
+
+namespace GameSparks.Api.Requests{
+	}
+	
+
+namespace GameSparks.Api.Messages {
+
+
+}
