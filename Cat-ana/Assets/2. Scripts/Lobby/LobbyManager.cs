b59e3482ce0d750fc4ef0ba34c1a07a7c5af0ed8
Trodek <ibanmas@gmail.com>
2017-06-14 20:50:45 +0200

    Store match connection info and create match session

--- Cat-ana/Assets/2. Scripts/Lobby/LobbyManager.cs
+++ Cat-ana/Assets/2. Scripts/Lobby/LobbyManager.cs
@@ -3,17 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System.Linq;
+using GameSparks.RT;
 
 public class LobbyManager : MonoBehaviour {
 
     public Button PlayBttn;
     public Matchmaking MM_Manager;
 
+    private MatchInfo match;
+
+    public MatchInfo GetMatchInfo()
+    {
+        return match;
+    }
+
+    private GameSparksRTUnity gameSparksRTunity;
+
+    public GameSparksRTUnity GetRTSession()
+    {
+        return gameSparksRTunity;
+    }
+
     private void OnMatchFound(GameSparks.Api.Messages.MatchFoundMessage _message)
     {
         Debug.Log("Match Found!...");
 
-        Debug.Log("Match Found...");
+        match = new MatchInfo(_message);
+
+        gameSparksRTunity.Configure(_message,
+            (peerID) => { OnPlayerConnectedToGame(peerID); },
+            (peerID) => { OnPlayerDisconnected(peerID); },
+            (ready) => { OnRTReady(ready); },
+            (packet) => { OnPacketReceived(packet); });
+        gameSparksRTunity.Connect();
+
+        //Uncoment that to Debug the Match info
+        /*Debug.Log("Match Found...");
         Debug.Log("Host URL:" + _message.Host);
         Debug.Log("Port:" + _message.Port);
         Debug.Log("Access Token:" + _message.AccessToken);
@@ -24,6 +49,40 @@ public class LobbyManager : MonoBehaviour {
         foreach (GameSparks.Api.Messages.MatchFoundMessage._Participant player in _message.Participants)
         {
             Debug.Log("Player:" + player.PeerId + " User Name:" + player.DisplayName); // add the player number and the display name to the list
+        }*/
+
+
+    }
+
+    private void OnPlayerConnectedToGame(int _peerId)
+    {
+        Debug.Log("GSM| Player Connected, " + _peerId);
+    }
+
+    private void OnPlayerDisconnected(int _peerId)
+    {
+        Debug.Log("GSM| Player Disconnected, " + _peerId);
+    }
+
+    private void OnRTReady(bool _isReady)
+    {
+        if (_isReady)
+        {
+            Debug.Log("GSM| RT Session Connected...");
+        }
+
+    }
+
+    private void OnPacketReceived(RTPacket _packet)
+    {
+    }
+
+    private void OnMatchReady(bool _isReady)
+    {
+        if(_isReady)
+        {
+            Debug.Log("Connected with the Match...");
+            //Change to game scene
         }
     }
 
---
+++ Cat-ana/Assets/2. Scripts/Match/MatchInfoClasses.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameSparks.Api.Messages;
+
+public class MatchInfo
+{
+    private string hostURL;
+    public string GetHostURL() { return this.hostURL; }
+
+    private string acccessToken;
+    public string GetAccessToken() { return this.acccessToken; }
+
+    private int portID;
+    public int GetPortID() { return this.portID; }
+
+    private string matchID;
+    public string GetMatchID() { return this.matchID; }
+
+    private List<Player> playerList = new List<Player>();
+    public List<Player> GetPlayerList()
+    {
+        return playerList;
+    }
+
+    public MatchInfo(MatchFoundMessage _message)
+    {
+        portID = (int)_message.Port;
+        hostURL = _message.Host;
+        acccessToken = _message.AccessToken;
+        matchID = _message.MatchId;
+        // we loop through each participant and get their peerId and display name //
+        foreach (MatchFoundMessage._Participant p in _message.Participants)
+        {
+            playerList.Add(new Player(p.DisplayName, p.Id, (int)p.PeerId));
+        }
+    }
+
+    public class Player
+    {
+        public Player(string _displayName, string _id, int _peerId)
+        {
+            this.displayName = _displayName;
+            this.id = _id;
+            this.peerId = _peerId;
+        }
+
+        public string displayName;
+        public string id;
+        public int peerId;
+        public bool isOnline;
+    }
+}
