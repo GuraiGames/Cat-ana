611c4cba8f6502d575406de5df85047658f686bd
rogerta97 <rogerteello97@gmail.com>
2017-06-14 14:10:13 +0200

    Login window WIP

--- Cat-ana/Assets/2. Scripts/Server/LoginBttn.cs
+++ Cat-ana/Assets/2. Scripts/Server/LoginBttn.cs
@@ -7,7 +7,14 @@ public class LoginBttn : MonoBehaviour
 {
 
     public Text user_name, password;
+
     public GameObject panel;
+    public GameObject user_name_go;
+    public GameObject password_go;
+    public GameObject register_go;
+    public GameObject login_go;
+
+    private int panel_message;
 
     public void LoginBttnClick()
     {
@@ -17,7 +24,13 @@ public class LoginBttn : MonoBehaviour
             {
                 if (response.HasErrors)
                 {
+                    user_name_go.SetActive(false);
+                    password_go.SetActive(false);
+                    register_go.SetActive(false);
+                    login_go.SetActive(false);
+
                     panel.SetActive(true);
+                    
                 }
                 else
                 {
