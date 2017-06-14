e259063fb42ea7087a42e8c57f0fffd8924203d9
Simón Stoyanov Beltrán <simon.stoyanov@hotmail.com>
2017-06-14 16:20:53 +0100

    Music can be toggled and Scrollbar WIP

---
+++ Cat-ana/Assets/2. Scripts/ChangeMusic.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMusic : MonoBehaviour {
+
+    private float current_volume = -1f;
+
+    public void ToggleMusic()
+    {
+        if (AudioListener.volume == 0)
+        {
+            AudioListener.volume = current_volume;
+        }   
+        else
+        {
+            if (current_volume == -1f)
+            {
+                current_volume = AudioListener.volume;
+            }
+            AudioListener.volume = 0;
+        }
+    }
+	
+    public void ScrollMusic()
+    {
+
+    }
+}
