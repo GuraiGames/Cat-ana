9f67751b49f536a81a232c1cbd7cad351c962c63
Trodek <ibanmas@gmail.com>
2017-06-14 19:42:14 +0200

    Matchmaking WIP

---
+++ Cat-ana/Assets/2. Scripts/Lobby/LobbyManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class LobbyManager : MonoBehaviour {
+
+    public Button PlayBttn;
+    public Matchmaking MM_Manager;
+
+    private void OnMatchFound(GameSparks.Api.Messages.MatchFoundMessage _message)
+    {
+        Debug.Log("Match Found!...");
+
+        Debug.Log("Match Found...");
+        Debug.Log("Host URL:" + _message.Host);
+        Debug.Log("Port:" + _message.Port);
+        Debug.Log("Access Token:" + _message.AccessToken);
+        Debug.Log("MatchId:" + _message.MatchId);
+        Debug.Log("Opponents:" + _message.Participants.Count());
+        Debug.Log("_________________");
+       
+        foreach (GameSparks.Api.Messages.MatchFoundMessage._Participant player in _message.Participants)
+        {
+            Debug.Log("Player:" + player.PeerId + " User Name:" + player.DisplayName); // add the player number and the display name to the list
+        }
+    }
+
+    void Start()
+    {
+        PlayBttn.onClick.AddListener(() =>
+        {
+            MM_Manager.NR_4PMatchMaking();
+            Debug.Log("Serching For Players..");
+        });
+
+        GameSparks.Api.Messages.MatchNotFoundMessage.Listener = (message) =>
+        {
+            Debug.Log("No Match Found...");
+        };
+
+        GameSparks.Api.Messages.MatchFoundMessage.Listener += OnMatchFound;
+    }
+}
---
+++ Cat-ana/Assets/2. Scripts/Server/Matchmaking.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Matchmaking : MonoBehaviour {
+
+	public void NR_4PMatchMaking()
+    {
+        new GameSparks.Api.Requests.MatchmakingRequest().SetMatchShortCode("4P_NRMATCH")
+            .SetSkill(0)
+            .Send((response) =>
+            {
+                if(response.HasErrors)
+                {
+                    Debug.LogError("MatchMakingError \n" + response.Errors.JSON);
+                }
+            });
+    }
+}
