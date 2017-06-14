c5fe31532d0466fcf1714a3c1c178974736e1c95
Simón Stoyanov Beltrán <simon.stoyanov@hotmail.com>
2017-06-14 18:19:40 +0100

    change display name implemented
    Found bug about password, fix in progress
    Play button created

---
+++ Cat-ana/Assets/2. Scripts/Server/ChangeNameBttn.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using GameSparks.Api.Requests;
+
+public class ChangeNameBttn : MonoBehaviour {
+
+    public Text display_name, old_password, new_password;
+    public GameObject panel;
+    public GameObject main_window;
+
+    public void ChangeNameBttnClick()
+    {
+        new ChangeUserDetailsRequest().SetDisplayName(display_name.text)
+            .SetNewPassword(new_password.text)
+            .SetOldPassword(old_password.text)
+            .Send((response) => {
+                if (!response.HasErrors)
+                    {
+                        main_window.SetActive(false);
+                    }
+                    else
+                    {
+                        panel.SetActive(true);
+                    }
+            });
+    }
+}
