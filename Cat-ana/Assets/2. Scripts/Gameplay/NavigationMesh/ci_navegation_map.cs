5b71b979145f9bf2ee75a50b8f536e274bd03300
Guillem Sunyer Caldú <gsunyercaldu@gmail.com>
2017-06-15 22:38:10 +0200

    Navigation map WIP

---
+++ Cat-ana/Assets/2. Scripts/Gameplay/NavigationMesh/ci_navegation_map.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(navegation_map))]
+public class ce_navegation_map : Editor
+{
+    float pos_x, pos_y, pos_z = 0;
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        navegation_map myScript = (navegation_map)target;
+
+        EditorGUILayout.Separator();
+        EditorGUILayout.LabelField("NAVEGATION MESH CONTROL");
+
+        GUILayout.BeginHorizontal();
+        EditorGUILayout.LabelField("x: ");
+        pos_x = EditorGUILayout.FloatField(pos_x);
+        GUILayout.EndHorizontal();
+
+        GUILayout.BeginHorizontal();
+        EditorGUILayout.LabelField("y: ");
+        pos_y = EditorGUILayout.FloatField(pos_y);
+        GUILayout.EndHorizontal();
+
+        GUILayout.BeginHorizontal();
+        EditorGUILayout.LabelField("z: ");
+        pos_y = EditorGUILayout.FloatField(pos_z);
+        GUILayout.EndHorizontal();
+
+        if (GUILayout.Button("Add point"))
+        {
+            if (myScript.nav_map_point == null || myScript.nav_map_points_parent == null)
+            {
+                Debug.LogError("Nav map point or nav mav point parent not set to an object on the inspector");
+                return;
+            }
+
+            GameObject point = Instantiate(myScript.nav_map_point);
+            point.transform.position = new Vector3(pos_x, pos_y, pos_z);
+            point.transform.parent = myScript.nav_map_points_parent.transform;
+        }
+    }
+
+}
