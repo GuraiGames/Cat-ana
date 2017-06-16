e5714eb9e13801e7781d48ee373e21b70d0178e7
Simón Stoyanov Beltrán <simon.stoyanov@hotmail.com>
2017-06-17 00:51:30 +0100

    local timer implemented (needs to be from the server)

--- Cat-ana/Assets/2. Scripts/Lobby/ScrollSpawn.cs
+++ Cat-ana/Assets/2. Scripts/Lobby/ScrollSpawn.cs
@@ -1,37 +1,62 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class ScrollSpawn : MonoBehaviour {
+public class ScrollSpawn : MonoBehaviour
+{
 
     public GameObject[] scroll_go;
+    public Text[] timer_txt;
 
-    [SerializeField]
-    private float distance_x = 0;
-
-    [SerializeField]
-    private float distance_y = 0;
+    public float[] timer = new float[4];
 
     // Use this for initialization
-    void Start () {
-       for (int i = 0; i < scroll_go.Length; i++)
+    void Start()
+    {
+        for (int i = 0; i < scroll_go.Length; i++)
         {
             scroll_go[i].SetActive(false);
         }
     }
-	
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetKeyDown(KeyCode.A))
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
         {
             for (int i = 0; i < scroll_go.Length; i++)
             {
                 if (!scroll_go[i].activeSelf)
                 {
                     scroll_go[i].SetActive(true);
+                    timer[i] = 3600*2;
                     break;
                 }
             }
         }
-	}
+
+        for (int i = 0; i < scroll_go.Length; i++)
+        {
+            if (scroll_go[i].activeSelf)
+            {
+                string hours;
+                string minutes = ((timer[i] % 3600) / 60).ToString("00");
+
+                if (minutes == "60")
+                {
+                    hours = Mathf.Floor(timer[i] / 3600 + 1).ToString("00");
+                    minutes = "00";
+                }
+                else
+                {
+                    hours = Mathf.Floor(timer[i] / 3600).ToString("00");      
+                }
+
+                timer_txt[i].text = hours + ":" + minutes;
+                timer[i] -= Time.deltaTime;
+            }
+        }
+
+    }
 }
