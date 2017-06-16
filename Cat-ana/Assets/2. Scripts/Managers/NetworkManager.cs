2572e16c2cd16aaf40dde507651f5c82ca4e2cc4
Trodek <ibanmas@gmail.com>
2017-06-16 15:18:54 +0200

    solved issue with packet 102 timing

--- Cat-ana/Assets/2. Scripts/Managers/NetworkManager.cs
+++ Cat-ana/Assets/2. Scripts/Managers/NetworkManager.cs
@@ -10,6 +10,8 @@ public class NetworkManager : MonoBehaviour
     //Match info
     public MatchInfo match;
 
+    private MatchManager match_manager = null;
+
     public MatchInfo GetMatchInfo()
     {
         return match;
@@ -59,7 +61,7 @@ public class NetworkManager : MonoBehaviour
     // Packet Handling
     public void OnPacketReceived(RTPacket _packet)
     {
-        switch(_packet.OpCode)
+        switch (_packet.OpCode)
         {
             // Add here a case for each OpCode. OpCodes documentation is available at drive/Code/OpCodes
             case 100: //All players connected to the server
@@ -68,13 +70,17 @@ public class NetworkManager : MonoBehaviour
                 break;
             case 101: //Sync Clock
                 {
-                    MatchManager match_manager = GameObject.FindGameObjectWithTag("MatchManager").GetComponent<MatchManager>();
+                    if (match_manager == null)
+                        match_manager = GameObject.FindGameObjectWithTag("MatchManager").GetComponent<MatchManager>();
                     match_manager.CalculateConnectionDelays(_packet);
                     break;
                 }
-            case 102://player match info
+            case 102://player match initial info
                 {
                     Debug.Log("Recieved player " + _packet.Data.GetString(1) + " info");
+                    if (match_manager == null)
+                        match_manager = GameObject.FindGameObjectWithTag("MatchManager").GetComponent<MatchManager>();
+                    match_manager.SetPlayersInfo(_packet);
                     break;
                 }
         }
@@ -88,5 +94,4 @@ public class NetworkManager : MonoBehaviour
             Debug.Log("Connected with the Match...");
         }
     }
-
 }
