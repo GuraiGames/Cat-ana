142d554a7b2dd391ef4ba9f385bd39a6976b02e8
Guillem Sunyer Caldú <gsunyercaldu@gmail.com>
2017-06-18 16:25:07 +0200

    Added error text to register page

--- Cat-ana/Assets/2. Scripts/UI/RegisterUI.cs
+++ Cat-ana/Assets/2. Scripts/UI/RegisterUI.cs
@@ -8,7 +8,10 @@ public class RegisterUI : MonoBehaviour
     private GameManager game_manager;
 
     [SerializeField]
-    private int password_min_lenght;
+    private int fields_min_lenght;
+
+    [SerializeField]
+    private int fields_max_lenght;
 
     [SerializeField]
     private InputField username;
@@ -19,9 +22,31 @@ public class RegisterUI : MonoBehaviour
     [SerializeField]
     private InputField password;
 
+    [SerializeField]
+    private Text error_text;
+
     void OnEnable()
     {
         game_manager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+    }
+
+    void SetErrorText(string text)
+    {
+        error_text.gameObject.SetActive(true);
+        error_text.text = text;
+    }
+
+    void CleanErrorText()
+    {
+        error_text.gameObject.SetActive(false);
+        error_text.text = "error text";
+    }
+
+    void ClearFields()
+    {
+        username.text = "";
+        display_name.text = "";
+        password.text = "";
     }
 
     public void ButtonPressed()
@@ -30,12 +55,22 @@ public class RegisterUI : MonoBehaviour
 
         if (username.text == "" || display_name.text == "" && password.text == "")
         {
-            Debug.Log("Register error: There is an empty field");
+            Debug.Log("Register error: There are empty fields");
+            SetErrorText("Register error: There are empty fields");
             return;
         }
-        if(password.text.Length < password_min_lenght)
+
+        if(password.text.Length < fields_min_lenght || username.text.Length < fields_min_lenght || display_name.text.Length < fields_min_lenght)
         {
-            Debug.Log("Register error: Password should be longuer than " + password_min_lenght + " words");
+            Debug.Log("Register error: Fields should be longuer than " + fields_min_lenght + " words");
+            SetErrorText("Register error: Fields should be longuer than " + fields_min_lenght + " words");
+            return;
+        }
+
+        if(password.text.Length > fields_max_lenght || username.text.Length > fields_max_lenght || display_name.text.Length > fields_max_lenght)
+        {
+            Debug.Log("Register error: Fields should be less than " + fields_max_lenght + " words");
+            SetErrorText("Register error: Fields should be less than " + fields_max_lenght + " words");
             return;
         }
 
@@ -48,12 +83,16 @@ public class RegisterUI : MonoBehaviour
             if (response.HasErrors)
             {
                 Debug.Log("Register error: " + response.Errors.JSON.ToString());
+                SetErrorText("Register error: " + response.Errors.JSON.ToString());
             }
             else
             {
                 Debug.Log("Register succes");
                 game_manager.GetUIManager().EnableWindow("login_register");
                 game_manager.GetUIManager().DisableWindow("register");
+
+                CleanErrorText();
+                ClearFields();
             }
         }
         );
