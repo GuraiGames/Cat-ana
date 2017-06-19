672fd0dfe4686cc7698348e61a9d26159a40a5dd
Guillem Sunyer Caldú <gsunyercaldu@gmail.com>
2017-06-20 00:35:31 +0200

    Added event manager

---
+++ Cat-ana/Assets/2. Scripts/Managers/EventManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventManager : MonoBehaviour 
+{
+    private Dictionary<string, UnityEvent> eventDictionary = null;
+
+    // Private functions
+    private void Start () 
+	{        
+        eventDictionary = new Dictionary<string, UnityEvent>();
+    }
+
+    public void StartListening(string eventName, UnityAction listener)
+    {
+        UnityEvent thisEvent = null;
+        if (eventDictionary.TryGetValue(eventName, out thisEvent))
+        {
+            thisEvent.AddListener(listener);
+        }
+        else
+        {
+            thisEvent = new UnityEvent();
+            thisEvent.AddListener(listener);
+            eventDictionary.Add(eventName, thisEvent);
+        }
+    }
+
+    public void StopListening(string eventName, UnityAction listener)
+    {
+        UnityEvent thisEvent = null;
+
+        if (eventDictionary.TryGetValue(eventName, out thisEvent))
+        {
+            thisEvent.RemoveListener(listener);
+        }
+    }
+
+    public void TriggerEvent(string eventName)
+    {
+        UnityEvent thisEvent = null;
+
+        if (eventDictionary.TryGetValue(eventName, out thisEvent))
+        {
+            thisEvent.Invoke();
+        }
+    }
+}
