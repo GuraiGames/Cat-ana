5bef3e2a5c83ddaad9eba3efd8c9241dd8efa8c6
Simón Stoyanov Beltrán <simon.stoyanov@hotmail.com>
2017-06-27 18:08:41 +0100

    top 10 scrollable panels set

--- Cat-ana/Assets/2. Scripts/Lobby/Leaderboard.cs
+++ Cat-ana/Assets/2. Scripts/Lobby/Leaderboard.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Leaderboard : MonoBehaviour {
 
     public GameObject PlayerScore;
+    public GameObject[] Top10 = new GameObject[10];
 
 	// Use this for initialization
 	void Start () {
