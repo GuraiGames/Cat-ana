b770d455b047e6a6d73b375c21b89cd9a2012461
Simón Stoyanov Beltrán <simon.stoyanov@hotmail.com>
2017-07-12 13:33:59 +0100

    changed the base code for facebook login

--- Cat-ana/Assets/2. Scripts/UI/FacebookLoginUI.cs
+++ Cat-ana/Assets/2. Scripts/UI/FacebookLoginUI.cs
@@ -1,55 +1,99 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-using GameSparks.Core;
+using System.Linq;
+
 using Facebook;
 using Facebook.Unity;
-using FacebookGames;
+using GameSparks.Api;
+using GameSparks.Api.Requests;
+using GameSparks.Api.Responses;
 
 public class FacebookLoginUI : MonoBehaviour {
 
-    public void FacebookConnect_bttn()
+    //General player information variables
+    public string fbAuthToken = "";
+
+    void Awake()
     {
-        Debug.Log("Connecting Facebook With GameSparks...");// first check if FB is ready, and then login //
-                                                            // if it's not ready we just init FB and use the login method as the callback for the init method //
-        if (!FB.IsInitialized)
+        //load authtoken from playerprefs and see if it's been filled in
+        Load();
+        if (fbAuthToken != "")
         {
-            Debug.Log("Initializing Facebook...");
-            FB.Init(ConnectGameSparksToGameSparks, null);
-        }
-        else
-        {
-            FB.ActivateApp();
-            ConnectGameSparksToGameSparks();
+            GameSparksFBLogBackIn();
         }
     }
 
-    ///<summary>
-    ///This method is used as the delegate for FB initialization. It logs in FB
-    /// </summary>
-    private void ConnectGameSparksToGameSparks()
+    public void CallFBInit()
     {
-        if (FB.IsInitialized){
-            FB.ActivateApp();
-            Debug.Log("Logging Into Facebook...");
-            var perms = new List<string>() { "public_profile", "email", "user_friends" };
-            FB.LogInWithReadPermissions(perms, (result) => {
-                if (FB.IsLoggedIn){
-                    Debug.Log("Logged In, Connecting GS via FB.."); new GameSparks.Api.Requests.FacebookConnectRequest()
-                        .SetAccessToken(AccessToken.CurrentAccessToken.TokenString)
-                        .SetDoNotLinkToCurrentPlayer(false)// we don't want to create a new account so link to the player that is currently logged in
-                        .SetSwitchIfPossible(true)//this will switch to the player with this FB account id they already have an account from a separate login
-                        .Send((fbauth_response) => {
-                            Debug.LogWarning(fbauth_response.Errors.JSON);//if we have errors, print them out
-                        });
-                }
-        else{
-                    Debug.LogWarning("Facebook Login Failed:" + result.Error);
-                }
-            });// lastly call another method to login, and when logged in we have a callback
-        }
-    else{
-            FacebookConnect_bttn();// if we are still not connected, then try to process again
+        //initialize facebook
+        FB.Init(this.OnInitComplete, this.OnHideUnity);
+    }
+
+    private void OnInitComplete()
+    {
+        Debug.Log("FB.Init completed: Is user logged in? " + FB.IsLoggedIn);
+        CallFBLogin();
+    }
+
+    private void CallFBLogin()
+    {
+        FB.LogInWithReadPermissions(new List<string>() { "public_profile", "email" }, GameSparksFBLogin);
+    }
+
+    private void GameSparksFBLogin(ILoginResult result)
+    {
+        if (FB.IsLoggedIn)
+        {
+            fbAuthToken = AccessToken.CurrentAccessToken.TokenString;
+            Save();
+            new FacebookConnectRequest()
+                .SetAccessToken(AccessToken.CurrentAccessToken.TokenString)
+                .SetSwitchIfPossible(true)
+                .SetSyncDisplayName(true)
+                .Send((response) => {
+                    if (response.HasErrors)
+                    {
+                        Debug.Log("Something failed when connecting with Facebook - " + result.Error);
+                    }
+                    else
+                    {
+                        Debug.Log("Gamesparks Facebook login successful");
+                    }
+                });
         }
     }
+
+    private void GameSparksFBLogBackIn()
+    {
+        new FacebookConnectRequest()
+            .SetAccessToken(fbAuthToken)
+            .Send((response) => {
+                if (response.HasErrors)
+                {
+                    Debug.Log("Something failed when connecting with Facebook on log back in");
+                }
+                else
+                {
+                    Debug.Log("Gamesparks Facebook login successful on log back in");
+                }
+            });
+    }
+
+    private void OnHideUnity(bool isGameShown)
+    {
+
+    }
+
+    void Save()
+    {
+        PlayerPrefs.SetString("fbAuthToken", fbAuthToken);
+    }
+
+    void Load()
+    {
+        fbAuthToken = PlayerPrefs.GetString("fbAuthToken");
+    }
+
 }
